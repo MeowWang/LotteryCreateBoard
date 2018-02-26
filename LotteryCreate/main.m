1b617c6bb45ce987a0b18221be1972337e00c12f
王雪利 <Monalisa@monawang-mc0.local>
2018-02-26 15:18:19 +0800

    Initial Commit

---
+++ LotteryCreate/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LotteryCreate
+//
+//  Created by Monalisa on 2018/2/26.
+//  Copyright © 2018年 Monalisa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
